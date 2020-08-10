52d2454aa559575f5c6de4cf2c62e6516d3492b0
Nicolas Mace <dev.nicolas@gmail.com>
2020-08-10 09:33:52 +0200

    Add index.php pages and stylesheets.

---
+++ public/admin/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8"/>
+    <title>GBI - Global Bank International</title>
+    <link href="../stylesheets/admin.css" rel="stylesheet" media="all" />
+  </head>
+  <body>
+
+    <header>
+      <h1>GBI Admin Area</h1>
+    </header>
+
+    <navigation>
+      <ul>
+        <li><a href="index.php">Menu</a></li>
+      </ul>
+    </navigation>
+
+    <div id="content"></div>
+
+    <footer>&copy; <?= date('Y'); ?> Globe Bank</footer>
+    
+  </body>
+</html>

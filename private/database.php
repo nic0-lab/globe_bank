631d5c9331208919a93d40563d0360798e102925
Nicolas Mace <dev.nicolas@gmail.com>
2020-08-13 15:06:53 +0200

    Handle connections and database query errors.

--- private/database.php
+++ private/database.php
@@ -4,6 +4,7 @@ require_once('db_credentials.php');
 
 function db_connect() {
     $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+    confirm_db_connect();
     return $connection;
 }
 
@@ -14,5 +15,21 @@ function db_disconnect($connection)
     }
 }
 
+function confirm_db_connect()
+{
+    if (mysqli_connect_errno()) {
+        $msg = 'Database connection failed: ';
+        $msg .= mysqli_connect_error();
+        $msg .= ' (' . mysqli_connect_errno() . ')';
+        exit($msg);
+    }
+}
+
+function confirm_result_set($result_set)
+{
+    if (!$result_set) {
+        exit('Database query failed.');
+    }
+}
 
 ?>
